508300950e50449e81a3f22aaadd0d95758b4159
denisgodhani <godhanidenis@gmail.com>
2022-03-21 11:34:07 +0530

    Weather Forcasting Test With NgRX

---
+++ apps/task/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+	selector: 'bp-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss'],
+	changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class AppComponent {
+	title = 'task';
+	// Testing Git Commit and Push Permission
+}
